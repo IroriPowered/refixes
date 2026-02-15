b7d7dda9274c48a021a3e7313e14224926e800c7
KabanFriends <yuukun2004@protonmail.ch>
2026-02-16 01:56:18 +0900

    feat: logs class for main plugin

---
+++ plugin/src/main/java/cc/irori/refixes/util/Logs.java
@@ -0,0 +1,16 @@
+package cc.irori.refixes.util;
+
+import com.hypixel.hytale.logger.HytaleLogger;
+
+public final class Logs {
+
+    private static final String LOGGER_NAME = "Refixes";
+
+    // Private constructor to prevent instantiation
+    private Logs() {
+    }
+
+    public static HytaleLogger logger() {
+        return HytaleLogger.get(LOGGER_NAME);
+    }
+}
