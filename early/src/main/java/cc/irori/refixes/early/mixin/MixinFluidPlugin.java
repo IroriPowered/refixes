cf2e68490da480f9a52e9c1732eb7f0ee8b4a6b7
KabanFriends <yuukun2004@protonmail.ch>
2026-02-16 19:50:07 +0900

    feat: port FluidFixer as mixin (not configurable yet)

---
+++ early/src/main/java/cc/irori/refixes/early/mixin/MixinFluidPlugin.java
@@ -0,0 +1,26 @@
+package cc.irori.refixes.early.mixin;
+
+import com.hypixel.hytale.builtin.fluid.FluidPlugin;
+import com.hypixel.hytale.server.core.universe.world.events.ChunkPreLoadProcessEvent;
+import java.util.function.Consumer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(FluidPlugin.class)
+public class MixinFluidPlugin {
+
+    @ModifyArg(
+            method = "setup",
+            at =
+                    @At(
+                            value = "INVOKE",
+                            target =
+                                    "Lcom/hypixel/hytale/event/EventRegistry;registerGlobal(Lcom/hypixel/hytale/event/EventPriority;Ljava/lang/Class;Ljava/util/function/Consumer;)Lcom/hypixel/hytale/event/EventRegistration;"),
+            index = 2)
+    private Consumer<ChunkPreLoadProcessEvent> refixes$disableFluidChunkPreProcess(
+            Consumer<ChunkPreLoadProcessEvent> consumer) {
+        // TODO: Make this behavior configurable (needs early plugin config)
+        return event -> {};
+    }
+}
