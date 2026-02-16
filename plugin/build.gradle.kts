f4bfa7563800d306c474a1bb47f429b1408e4f18
KabanFriends <yuukun2004@protonmail.ch>
2026-02-16 10:21:11 +0900

    build: add guava as main plugin dependency

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,7 +1,15 @@
 dependencies {
     compileOnly(libs.hytale)
+
+    implementation(libs.guava)
 }
 
 base {
     archivesName.set("refixes-plugin")
+}
+
+tasks {
+    shadowJar {
+        relocate("com.google.common", "cc.irori.refixes.lib.com.google.common")
+    }
 }
