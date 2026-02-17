72d317f3a4f9b6f363c16be499f9dd6f3fff402d
KabanFriends <yuukun2004@protonmail.ch>
2026-02-17 18:09:18 +0900

    start work on shared instance

---
+++ early/src/main/java/cc/irori/refixes/early/mixin/MixinChunkSavingSystems.java
@@ -0,0 +1,42 @@
+package cc.irori.refixes.early.mixin;
+
+import cc.irori.refixes.early.util.SharedInstanceConstants;
+import com.hypixel.hytale.component.ArchetypeChunk;
+import com.hypixel.hytale.component.Store;
+import com.hypixel.hytale.server.core.universe.world.World;
+import com.hypixel.hytale.server.core.universe.world.chunk.ChunkFlag;
+import com.hypixel.hytale.server.core.universe.world.chunk.WorldChunk;
+import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore;
+import com.hypixel.hytale.server.core.universe.world.storage.component.ChunkSavingSystems;
+import com.llamalad7.mixinextras.sugar.Local;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ChunkSavingSystems.class)
+public class MixinChunkSavingSystems {
+
+    @Inject(
+            method = "tryQueue",
+            at =
+                    @At(
+                            value = "INVOKE",
+                            target =
+                                    "Lcom/hypixel/hytale/component/ArchetypeChunk;getReferenceTo(I)Lcom/hypixel/hytale/component/Ref;"),
+            cancellable = true)
+    private static void refixes$skipSavingSharedInstanceChunks(
+            int index,
+            ArchetypeChunk<ChunkStore> archetypeChunk,
+            Store<ChunkStore> store,
+            CallbackInfo ci,
+            @Local(name = "worldChunkComponent") WorldChunk worldChunkComponent) {
+        if (worldChunkComponent.is(ChunkFlag.ON_DISK)) {
+            World world = store.getExternalData().getWorld();
+            if (world.getName().startsWith(SharedInstanceConstants.SHARED_INSTANCE_PREFIX)) {
+                worldChunkComponent.consumeNeedsSaving();
+                ci.cancel();
+            }
+        }
+    }
+}
---
+++ early/src/main/java/cc/irori/refixes/early/util/SharedInstanceConstants.java
@@ -0,0 +1,9 @@
+package cc.irori.refixes.early.util;
+
+public final class SharedInstanceConstants {
+
+    public static final String SHARED_INSTANCE_PREFIX = "instance-shared-";
+
+    // Private constructor to prevent instantiation
+    private SharedInstanceConstants() {}
+}
