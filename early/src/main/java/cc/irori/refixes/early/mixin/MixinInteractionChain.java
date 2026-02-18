41ab97dcbfd21a656fa355c48b8fbe7e4a871954
KabanFriends <yuukun2004@protonmail.ch>
2026-02-19 03:33:44 +0900

    style: remove unused imports

--- early/src/main/java/cc/irori/refixes/early/mixin/MixinInteractionChain.java
+++ early/src/main/java/cc/irori/refixes/early/mixin/MixinInteractionChain.java
@@ -9,9 +9,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InteractionChain.class)
 public class MixinInteractionChain {
