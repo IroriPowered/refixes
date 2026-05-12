79104be2279a67e9b05a24619284ed415666bc87
Xytronix <32957125+Xytronix@users.noreply.github.com>
2026-05-12 11:03:50 +0200

    fix: cast to PluginBase for getCommandRegistry instead of @Shadow (Mixin can't shadow inherited methods)

--- early/src/main/java/cc/irori/refixes/early/mixin/MixinBuilderToolsPlugin.java
+++ early/src/main/java/cc/irori/refixes/early/mixin/MixinBuilderToolsPlugin.java
@@ -4,9 +4,8 @@ import cc.irori.refixes.early.copychunks.CopyChunksCommand;
 import cc.irori.refixes.early.copychunks.PasteChunksCommand;
 import com.hypixel.hytale.builtin.buildertools.BuilderToolsPlugin;
 import com.hypixel.hytale.server.core.command.system.CommandRegistry;
-import javax.annotation.Nonnull;
+import com.hypixel.hytale.server.core.plugin.PluginBase;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,13 +13,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(BuilderToolsPlugin.class)
 public abstract class MixinBuilderToolsPlugin {
 
-    @Shadow
-    @Nonnull
-    public abstract CommandRegistry getCommandRegistry();
-
     @Inject(method = "setup", at = @At("TAIL"))
     private void refixes$registerCopyChunks(CallbackInfo ci) {
-        CommandRegistry registry = getCommandRegistry();
+        CommandRegistry registry = ((PluginBase) (Object) this).getCommandRegistry();
         registry.registerCommand(new CopyChunksCommand());
         registry.registerCommand(new PasteChunksCommand());
     }
